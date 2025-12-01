9796ae8ad0d3343af80f08edbdd380b1906eeea2
RyanGumlia <ryan.gumlia@yale.edu>
2025-11-30 23:36:22 -0500

    polish

--- src/app/posts/walrus/page.tsx
+++ src/app/posts/walrus/page.tsx
@@ -26,16 +26,16 @@ export default function WalrusPage()
 			<p>
 				It seems sometimes that I am being suffocated by everything I have ever heard, read, and wrote. 
 				It seems that I am seeking a metaphor,<Footnote index={1} vis={vis}><i>Med.</i>, 19.</Footnote> 
-				seems that I live as I dream,<Footnote index={3} vis={vis}>Conrad, <i>Heart of Darkness</i>.</Footnote> 
-				seems I am clawing, gasping, and barely breaching<Footnote index={4} vis={vis}><i>Dev.</i>, 22.</Footnote> 
-				three things in this speech,<Footnote index={5} vis={vis}>WUDC 2025, Sydney C.</Footnote> 
-				two reasons why,<Footnote index={6} vis={vis}>"Writing about Oneself."</Footnote> 
-				or to become one?<Footnote index={7} vis={vis}><i>Dev.</i>, 1.</Footnote> 
-				No, scaring from the dead rat’s five more,<Footnote index={8} vis={vis}>AM.</Footnote> 
-				the V4,<Footnote index={9} vis={vis}>Ascent, "The Impaler."</Footnote> 
-				the face you keep in a jar by the door, who is this for?<Footnote index={10} vis={vis}>The Beatles, "Eleanor Rigby."</Footnote> 
-				All the hermetic logic,<Footnote index={11} vis={vis}>DZ</Footnote> 
-				where does it all belong? Probably not Here, There and Every—wait.<Footnote index={10} vis={vis}>The Beatles, "—where."</Footnote>  
+				seems that I live as I dream,<Footnote index={2} vis={vis}>Conrad, <i>Heart of Darkness</i>.</Footnote> 
+				seems I am clawing, gasping, and barely breaching<Footnote index={3} vis={vis}><i>Dev.</i>, 22.</Footnote> 
+				three things in this speech,<Footnote index={4} vis={vis}>WUDC 2025, Sydney C.</Footnote> 
+				two reasons why,<Footnote index={5} vis={vis}>"Writing about Oneself."</Footnote> 
+				or to become one?<Footnote index={6} vis={vis}><i>Dev.</i>, 1.</Footnote> 
+				No, scaring from the dead rat’s five more,<Footnote index={7} vis={vis}>AM.</Footnote> 
+				the V4,<Footnote index={8} vis={vis}>Ascent, "The Impaler."</Footnote> 
+				the face you keep in a jar by the door, who is this for?<Footnote index={9} vis={vis}>The Beatles, "Eleanor Rigby."</Footnote> 
+				All the hermetic logic,<Footnote index={10} vis={vis}>DZ.</Footnote> 
+				where does it all belong? Probably not Here, There and Every—wait.<Footnote index={11} vis={vis}>The Beatles, "—where."</Footnote>  
 				Let’s try that. Okay… I really want to stop the show, because I think you might like to know, yes! 
 				That the writer’s gonna right a wrong, and I want you all to read along, wow. 
 				So let me introduce to you the one and only—uh, who the fuck is Billy Shears?
@@ -68,35 +68,36 @@ export default function WalrusPage()
 			</p>
 			<p>
 				If you ask who is steering this ship, you ask whether such a totalizing intertextuality doesn’t efface some essence, 
-				doesn’t disintegrate some vocal muscle that no multiple of 42 can save. 
+				doesn’t disintegrate some vocal muscle that no multiple of 42<Footnote index={14} vis={vis}>Fairlife.</Footnote> can save. 
 				You point out that such deafening displays of allusion might hamstring the non-associative, 
 				foreclose novelty, even inspire LLM-type spirals into muddled on muddled on nothing. 
 				I shrug. I ask for a word that hasn’t been used. 
-				I point to Apollo.<Footnote index={14} vis={vis}><i>Star Trek</i>, "Who Mourns for Adonais?"</Footnote> 
-				I point to Alexander.<Footnote index={15} vis={vis}><i>Star Trek</i>, "Plato's Stepchildren."</Footnote> 
-				I extend my hand into a sharp metal spear<Footnote index={16} vis={vis}>Cameron, <i>Terminator II.</i></Footnote> 
-				and condemn you to the flute.<Footnote index={17} vis={vis}>Kurosawa, <i>Ran.</i></Footnote> 
-				I throw a handkerchief at you<Footnote index={18} vis={vis}>Shakespeare <i>Othello</i>.</Footnote> 
-				and ask whether you’ll fight back.<Footnote index={19} vis={vis}><i>Cheers</i>, "Homicidal Ham."</Footnote> 
+				I point to Apollo.<Footnote index={15} vis={vis}><i>Star Trek</i>, "Who Mourns for Adonais?"</Footnote> 
+				I point to Alexander.<Footnote index={16} vis={vis}><i>Star Trek</i>, "Plato's Stepchildren."</Footnote> 
+				I extend my hand into a sharp metal spear<Footnote index={17} vis={vis}>Cameron, <i>Terminator II.</i></Footnote> 
+				and condemn you to the flute.<Footnote index={18} vis={vis}>Kurosawa, <i>Ran.</i></Footnote> 
+				I throw a handkerchief at you<Footnote index={19} vis={vis}>Shakespeare <i>Othello</i>.</Footnote> 
+				and ask whether you’ll fight back.<Footnote index={20} vis={vis}><i>Cheers</i>, "Homicidal Ham."</Footnote> 
 				I sob as I bury my head in your neck as our hearts convulse in rhythm as.
 			</p>
 			<p style={{ textIndent: "2em" }}>
 				<i>2. What will be friction?</i>
 			</p>
 			<p>
-				Sophie sang “Losing My Religion” for one minute and fifty seconds.<Footnote index={19} vis={vis}>Wells, <i>Aftersun</i>.</Footnote> 
+				Sophie sang “Losing My Religion” for one minute and fifty seconds.<Footnote index={21} vis={vis}>Wells, <i>Aftersun</i>.</Footnote> 
 				Sophie I cannot understand. 
 				Can you weep over her? 
-				She was not pressured into a five-part “At The Copa.”<Footnote index={19} vis={vis}>DG, MK.</Footnote> 
-				She was not doused in vodka for holding the 40-second E.<Footnote index={19} vis={vis}>The Beatles, "A Day in the Life."</Footnote> 
-				When I think of Sophie, I am as though annihilated, as though paralyzed.<Footnote index={19} vis={vis}>Kierkegaard, <i>Fear and Trembling</i>.</Footnote>
+				She was not pressured into a five-part “At The Copa.”<Footnote index={22} vis={vis}>DG.</Footnote> 
+				She was not doused in vodka for holding the 40-second E.<Footnote index={23} vis={vis}>The Beatles, "A Day in the Life."</Footnote> 
+				When I think of Sophie, 
+				I am as though annihilated, as though paralyzed.<Footnote index={24} vis={vis}>Kierkegaard, <i>Fear and Trembling</i>.</Footnote>
 				I fall down—I am brought to my knees by the millions of bits that cry out in terror and are suddenly silenced. 
 				How do you not cry out! The spotlights her on fire, and we are breathing in, aren’t we all choking on the smoke? 
 				Not a drill nor a dream, Sophie cannot not lose her religion: she is one. 
 			</p>
 			<p>
 				Heavy is my head that voices desire. 
-				Uneasy should be any that hears the hitch.<Footnote index={19} vis={vis}>Shakespeare, <i>Henry IV</i>.</Footnote>  
+				Uneasy should be any that hears the hitch.<Footnote index={25} vis={vis}>Shakespeare, <i>Henry IV</i>.</Footnote>  
 				Our fabric is so light, so thinly strung that every assertion risks slicing us through a space’s time. 
 				Hooray that our swift faculties can calculate every line! 
 				That we can arm ourselves with deniabilities and hold the balance in a language of feints. 
@@ -106,20 +107,20 @@ export default function WalrusPage()
 				So few moves will be played, how could we be poorer for thinking every turn? Do we even need the end? 
 				Oh, Sophie, the end! We are safer here in machination. Can you really bear the toppled crown? 
 				See tearlessly him that couldn’t see far enough? 
-				If you do not shudder at Qe2,<Footnote index={18} vis={vis}>World Chess Championship, Final Tiebreak.</Footnote>  
+				If you do not shudder at Qe2,<Footnote index={26} vis={vis}>World Chess Championship 2023, Final Tiebreak.</Footnote>  
 				if Rg6 does not drench you in—does not make you tread nausea, you do not know me. 
 			</p>
 			<p>
-				We both know it is better to speak.<Footnote index={18} vis={vis}>Guadagnino.</Footnote> 
+				We both know it is better to speak.<Footnote index={27} vis={vis}>Guadagnino.</Footnote> 
 				I know it is better to join you on the shore than the sea. 
 				Where in pale blue and a hint of pink the sun dresses the coast. Where glimmers pool in rock-snagged tides and basketballs arc in a budding dusk. 
 				I ask if you have any water. Sorry, I mean I want water. A motley band strums to the waves nearby. Will you be going tomorrow? 
 				Jesus, no, I say I want to go with you. Their rhythm rises in the purple twilight, and my feet begin to tap. Fuck, no, those were mistakes! 
 				I didn’t hear you—what song? Please, I know I ordered the peppermint mocha, but he knew it, too. I asked for coffee, and I woke up to climb, please. 
 				A car door slams. The sands don’t feel his steps. He produces a bass guitar. 
-				Weber has arrived to make me dance until I die.<Footnote index={18} vis={vis}>DZ.</Footnote> I will speak anyway. 
+				Weber has arrived to make me dance until I die.<Footnote index={28} vis={vis}>DZ.</Footnote> I will speak anyway. 
 				I promise that my flesh will draw apart an abstract standing. 
-				A new splatter of fortitude, this, a praying face to the sea.<Footnote index={18} vis={vis}><i>Redwoods</i>R.</Footnote> 
+				A new splatter of fortitude, this, a praying face to the sea.<Footnote index={29} vis={vis}><i>Redwoods</i>R.</Footnote> 
 			</p>
 			<p style={{ textIndent: "2em" }}>
 				<i>3. What was optimization?</i>
@@ -156,8 +157,8 @@ export default function WalrusPage()
 				for return. I once argued that we are only conscious while we calculate, that there was fathomable no existence 
 				illegible to monologue. I had found in the modernists a trap to be escaped, but perhaps they really shared a new way to live. 
 				For December, I reach differently. 
-				Equipped with new lows of information access and a taste for sensory risk, I mean to abandon the self to a new being and hopefully other ones, too. 
-				I hope to think less, to be affect and be so. Ultimately, to leave a wake. 
+				Equipped with new lows of information access and a taste for sensory risk, I mean to abandon the self to a new being and hopefully others, too. 
+				I hope to think less, be affected, and leave a wake. 
 			</p>
     </PostShell>
   );
